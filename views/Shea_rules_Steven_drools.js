42919557db223d0dce099e3aaed19e230095baf2
SheaClose <SheaClose@gmail.com>
2017-11-10 10:03:20 -0600

    Nom Nom Nom Babies

---
+++ views/Shea_rules_Steven_drools.js
@@ -0,0 +1,3 @@
+console.log(
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nam placeat neque consequuntur numquam provident quis veniam debitis officiis? Quas a praesentium dolore cupiditate porro nesciunt ea tenetur delectus ab!"
+);
